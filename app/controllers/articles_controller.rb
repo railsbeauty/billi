2a895a3607f5f5e96917afc228e593f6f0270e9e
Vinay <vinay59.grewal@gmail.com>
2013-03-16 03:01:14 +0530

    added post functionality

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,36 @@
+class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+
+    def show
+      @article = Article.find(params[:id])
+    end
+
+	def new
+      @article = Article.new
+	end
+
+    def create
+      @article = Article.new(params[:article])
+      @article.save
+      redirect_to article_path(@article)
+    end
+
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      redirect_to action:  'index'	
+    end
+
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    def update
+     @article = Article.find(params[:id])
+     @article.update_attributes(params[:article])
+     redirect_to article_path(@article)
+end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mau::Application.config.secret_token = '4f50aad4b5d500e5f7a86a3645104bb19d62f8154cdb3faea9036adf71c92230477d0c8cb459d33739de065ef6653584c6a22f6c9d2ea585eaa2ce76e93b5f1d'
