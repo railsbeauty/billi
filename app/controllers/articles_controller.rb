5dc5d16ca0d59512755fe23db1a285c3a1d52ea8
vinay <vinay@vertisinfotech.com>
2013-04-17 11:13:58 +0530

    few changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
 
 	  def index
 		  @articles = Article.all(:order => "created_at DESC")
-      @article_titles = Article.select(:title).last(10)
+      @article_titles = Article.first(10)
 	  end
 
     def show
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Mau::Application.routes.draw do
   resources :tags
   resources :articles do
   resources :comments
+end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
